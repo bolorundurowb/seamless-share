32f82f1383949097cf95be0648a3266d3ec131de
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2025-02-24 00:46:38 +0100

    ft: add in the auth service helper methods

--- web/src/app/services/auth.service.ts
+++ web/src/app/services/auth.service.ts
@@ -7,6 +7,9 @@ import { firstValueFrom } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private baseUrl = environment.apiV1BaseUrl;
+  private tokenKey = 'ss-auth-token';
+  private tokenExpiryKey = 'ss-token-expiry';
+  private userKey = 'ss-user';
 
   constructor(private http: HttpClient) {
   }
@@ -17,5 +20,29 @@ export class AuthService {
 
   async register(req: RegisterReq): Promise<AuthRes> {
     return firstValueFrom(this.http.post<AuthRes>(`${this.baseUrl}/auth/register`, req));
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken() && !!this.getTokenExpiry() && this.getTokenExpiry()! > new Date();
+  }
+
+  persistAuth(authRes: AuthRes): void {
+    localStorage.setItem(this.tokenKey, authRes.accessToken);
+    localStorage.setItem(this.tokenExpiryKey, authRes.expiresAt);
+    localStorage.setItem(this.userKey, JSON.stringify(authRes.user));
+  }
+
+  clearAuth(): void {
+    localStorage.removeItem(this.tokenKey);
+    localStorage.removeItem(this.tokenExpiryKey);
+    localStorage.removeItem(this.userKey);
+  }
+
+  private getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  private getTokenExpiry(): Date | null {
+    return localStorage.getItem(this.tokenExpiryKey) ? new Date(localStorage.getItem(this.tokenExpiryKey)!) : null;
   }
 }
