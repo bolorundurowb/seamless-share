518f76f84a22aa8232f77b81d67f3e96eb465b0b
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2025-02-23 15:03:20 +0100

    ft: add in the url handler

--- web/src/app/home/home.page.ts
+++ web/src/app/home/home.page.ts
@@ -1,13 +1,33 @@
 import {Component} from '@angular/core';
 import {Title} from '@angular/platform-browser';
+import {NzTabComponent, NzTabSetComponent} from 'ng-zorro-antd/tabs';
+import {NzInputDirective, NzInputGroupComponent} from 'ng-zorro-antd/input';
+import {FormsModule} from '@angular/forms';
+import {NzButtonComponent} from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'ss-home',
   standalone: true,
   templateUrl: './home.page.html',
+  imports: [
+    NzTabSetComponent,
+    NzTabComponent,
+    NzInputGroupComponent,
+    FormsModule,
+    NzButtonComponent,
+    NzInputDirective
+  ]
 })
 export class HomePage {
+  sharedUrl?: string;
+  isSharedUrlValid = false;
+
   constructor(title: Title) {
     title.setTitle('Seamless Share | Home');
+  }
+
+  validateSharedUrl() {
+    const urlPattern = /^(https?:\/\/)?([\w\d-]+)\.([a-z]{2,})([\/\w\d.-]*)*\/?$/;
+    this.isSharedUrlValid = urlPattern.test(this.sharedUrl || '');
   }
 }
