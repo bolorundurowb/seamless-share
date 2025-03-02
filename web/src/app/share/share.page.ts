9435f19e157a40a762471409828bda339010b389
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2025-03-02 18:07:35 +0100

    ft: add the share page scaffold

---
+++ web/src/app/share/share.page.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'ss-share',
+  templateUrl: 'share.page.html',
+  styleUrl: 'share.page.scss',
+  standalone: true
+})
+export class SharePage {
+
+}
