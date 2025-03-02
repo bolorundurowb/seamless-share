ae8d841ccb26a1e90374c98c0e5c603ee90df23e
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2025-03-02 18:30:50 +0100

    ft: add share code retrieval for authenticated users

--- web/src/app/home/home.page.ts
+++ web/src/app/home/home.page.ts
@@ -1,11 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { NzTabComponent, NzTabSetComponent } from 'ng-zorro-antd/tabs';
-import { NzInputDirective, NzInputGroupComponent, NzTextareaCountComponent } from 'ng-zorro-antd/input';
-import { FormsModule } from '@angular/forms';
-import { NzButtonComponent } from 'ng-zorro-antd/button';
-import { AuthService } from '../services/auth.service';
-import { ShareService } from '../services/share.service';
+import {Title} from '@angular/platform-browser';
+import {NzTabComponent, NzTabSetComponent} from 'ng-zorro-antd/tabs';
+import {NzInputDirective, NzInputGroupComponent, NzTextareaCountComponent} from 'ng-zorro-antd/input';
+import {FormsModule} from '@angular/forms';
+import {NzButtonComponent} from 'ng-zorro-antd/button';
+import {AuthService} from '../services/auth.service';
+import {ShareService} from '../services/share.service';
 import {NgIf} from '@angular/common';
 import {UserRes} from '../types';
 
@@ -28,6 +28,7 @@ import {UserRes} from '../types';
 export class HomePage implements OnInit {
   isAuthenticated = false;
   user: UserRes | null = null;
+  shareCode?: string;
 
   sharedUrl?: string;
   isSharedUrlValid = false;
@@ -39,9 +40,14 @@ export class HomePage implements OnInit {
     title.setTitle('Seamless Share | Home');
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.isAuthenticated = this.authService.isAuthenticated();
     this.user = this.authService.getUser();
+
+    if (this.isAuthenticated) {
+      const ownedShare = await this.shareService.getOwnedShare();
+      this.shareCode = ownedShare.code;
+    }
   }
 
   validateSharedUrl() {
