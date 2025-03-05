cb1e5af0715df2e46011839cb3d49a31ef1a3f9a
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2025-03-04 18:14:53 -0800

    ft: first go at adding section support

--- web/src/app/components/section.component.ts
+++ web/src/app/components/section.component.ts
@@ -1,23 +1,50 @@
-import {Component, Input} from '@angular/core';
-
-@Component({
-  selector: 'ss-sections',
-  standalone: true,
-  template: `
-  <div>Hello</div>
-  `
-})
-export class SectionsComponent {
-
-}
+import {Component, ContentChild, ContentChildren, Input, QueryList, TemplateRef} from '@angular/core';
+import {NgForOf, NgIf, NgTemplateOutlet} from '@angular/common';
 
 @Component({
   selector: 'ss-section',
   standalone: true,
   template: `
-  <div>Hello</div>
+    <ng-template>
+      <ng-content></ng-content>
+    </ng-template>
   `
 })
 export class SectionComponent {
-@Input() title?: string;
+  @Input() title!: string;
+  @ContentChild(TemplateRef) template!: TemplateRef<any>;
+}
+
+
+@Component({
+  selector: 'ss-sections',
+  standalone: true,
+  imports: [
+    NgTemplateOutlet,
+    NgForOf,
+    NgIf
+  ],
+  template: `
+    <div class="sections-container">
+      <ng-container *ngFor="let section of sections; let i = index">
+        <div *ngIf="selectedSectionIndex === i" class="section-content">
+          <ng-container *ngTemplateOutlet="section.template"></ng-container>
+        </div>
+      </ng-container>
+      <div class="buttons">
+        <button *ngFor="let section of sections; let i = index"
+                (click)="selectSection(i)"
+                [class.active]="selectedSectionIndex === i">{{ section.title }}
+        </button>
+      </div>
+    </div>
+  `
+})
+export class SectionsComponent {
+  @ContentChildren(SectionComponent) sections!: QueryList<SectionComponent>;
+  selectedSectionIndex = 0;
+
+  selectSection(index: number) {
+    this.selectedSectionIndex = index;
+  }
 }
