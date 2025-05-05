d0f900ddad990773246d99e19d9304dbb29355ca
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2025-05-05 16:18:03 +0100

    ft: fix link categorizer

--- server/Utils/TextCategorizer.cs
+++ server/Utils/TextCategorizer.cs
@@ -6,6 +6,8 @@ public static partial class TextCategorizer
 {
     public static bool IsLink(string input) => !string.IsNullOrWhiteSpace(input) && UrlRegex().IsMatch(input);
 
-    [GeneratedRegex(@"^(http|https|ftp):\/\/[^\s/$.?#].[^\s]*$", RegexOptions.IgnoreCase, "en-NG")]
+    [GeneratedRegex(
+        @"^(?:(https?|ftp|ws):\/\/)?(?:([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/[^\s?#]*)?(?:\?[^#\s]*)?(?:#[^\s]*)?$",
+        RegexOptions.IgnoreCase, "en-NG")]
     private static partial Regex UrlRegex();
 }
