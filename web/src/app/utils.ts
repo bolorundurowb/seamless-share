72a03afbb541975bd4abea68f8d71a24b6a633b5
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2025-05-09 21:19:50 +0100

    ft: minor code cleanup

--- web/src/app/utils.ts
+++ web/src/app/utils.ts
@@ -1,16 +1,18 @@
-export const isFile = (item? : any): boolean => item && item.url && item.metadata?.mimeType;
+export const isFile = (item?: any): boolean => item && item.url && item.metadata?.mimeType;
 
-export const isLink = (item? : any): boolean => item && item.url && !item.metadata?.mimeType;
+export const isLink = (item?: any): boolean => item && item.url && !item.metadata?.mimeType;
 
-export const isText = (item? : any): boolean => item && item.content;
+export const isText = (item?: any): boolean => item && item.content;
 
 export const isUrlValid = (url?: string): boolean => {
-  if (!url) return false;
+  if (!url) {
+    return false;
+  }
 
   const urlPattern = /^(?:(https?|ftp|ws):\/\/)?(?:([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}|localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d+)?(?:\/[^\s?#]*)?(?:\?[^#\s]*)?(?:#[^\s]*)?$/;
   return urlPattern.test(url);
-}
+};
 
-export const extractErrorMessaging = (err: any): string | undefined =>  {
+export const extractErrorMessaging = (err: any): string | undefined => {
   return err.error?.message;
-}
+};
